c054860ccfe343e2b262673a8eeec3360930d033
Ryu KyungMin <ykmyou@naver.com>
2019-07-06 16:26:31 +0900

    add login module

--- Express/express_module.js
+++ Express/express_module.js
@@ -2,9 +2,13 @@ const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
+const mysql = require('mysql');
 
 const hostname = '127.0.0.1';
 const port = 3000;
+
+const dbHost = 'localhost';
+const dbPort = 3306;
 
 var app = express();
 var upload = multer();
@@ -22,16 +26,28 @@ app.listen(port, () => {
 });
 
 app.post('/login', (req, res, next) => {
-	
 	var user_id = req.body.id;
 	var user_password = req.body.password;
-	var info = [{ id : user_id }, { password : user_password }, {email : "bye"}, {name : "good"}]
-  	res.send(info);
+
+	var connection = mysql.createConnection({
+		host: dbHost,
+		post: dbPort,
+		user: 'root',
+		password: 'root',
+		database: 'vue_project'
+	});
+
+	var info;
+	connection.connect();
+	connection.query("SELECT * FROM tb_user WHERE id = '" + user_id + "' and password = '" + user_password + "'", function (err, rows, fields) {
+        connection.end();
+        if (!err) {
+            info = JSON.stringify(rows);
+            console.log(info);
+            res.send(info);
+        } else {
+            console.log('query error : ' + err);
+            res.send(null);
+        }
+    });
 });
-
-
-// app.get('/getarray', (req, res) => {
-
-//   var arr = [{ key : "hello "},{key : "hi"},{key : "bye"},{key : "good"}]
-//   res.send(arr);
-// });
